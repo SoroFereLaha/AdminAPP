e2654d74cf09a6054814d1526994a8bc6854ec44
Soro <soroferelaha01@gmail.com>
2023-06-24 17:14:06 +0100

    update du panel administration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1080,7 +1080,6 @@
 </div>
 
 
-
 <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl">
       <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-3">
